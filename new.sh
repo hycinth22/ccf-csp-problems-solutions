618254eaa92d429decf2c74b807affc571088b9f
风痕 <inkedawn@gmail.com>
2020-01-22 17:11:17 +0800

    add template

---
+++ new.sh
@@ -0,0 +1,8 @@
+id="$1"
+title="$2$1"
+folder="${title}"
+complile="${folder}/main.cpp"
+
+mkdir ./"${folder}"
+cp -r ./template/* ./"${folder}"/
+echo "add_executable(${id} ${complile})" >> CMakeLists.txt
---
+++ template/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef _DEBUG
+struct fakeOSType {
+    template<class contentType>
+    inline fakeOSType &operator<<(const contentType &cont) {return *this;}
+    inline fakeOSType &operator<<(ostream &op(ostream &)) {return *this;}
+} nullos;
+#define clog nullos
+#define cerr nullos
+#endif
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    
+}
